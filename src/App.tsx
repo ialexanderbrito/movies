19fc9bcd537457b12bbc7334a343c8bbc0899394
ialexanderbrito <xanderzinho26@gmail.com>
2022-10-24 19:27:27 -0300

    feat: add routes and import category provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,23 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { MainRoutes } from 'routes';
 
+import { CategoryProvider } from 'contexts/Category';
 import { ThemeProvider } from 'contexts/Theme';
 import { ToastProvider } from 'contexts/Toast';
+
+import 'react-alice-carousel/lib/alice-carousel.css';
+import 'react-circular-progressbar/dist/styles.css';
+import '@vime/core/themes/default.css';
 
 export function App() {
   return (
     <ToastProvider>
       <ThemeProvider>
-        <BrowserRouter>
-          <MainRoutes />
-        </BrowserRouter>
+        <CategoryProvider>
+          <BrowserRouter>
+            <MainRoutes />
+          </BrowserRouter>
+        </CategoryProvider>
       </ThemeProvider>
     </ToastProvider>
   );
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 
+import { Details } from 'pages/Details';
+import { DetailsTv } from 'pages/DetailsTv';
 import { Homepage } from 'pages/Home';
 
 export function MainRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
+      <Route path="/movie/:id" element={<Details />} />
+      <Route path="/series/:id" element={<DetailsTv />} />
     </Routes>
   );
 }
