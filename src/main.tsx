939f6f41b73e099534cf8fd4f0496d429453b65f
ialexanderbrito <xanderzinho26@gmail.com>
2022-10-24 19:10:33 -0300

    feat: add query client provider and query dev tools

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/global.scss';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+import { dev } from 'utils/environment';
+
 import { App } from './App';
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      {dev && <ReactQueryDevtools />}
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
